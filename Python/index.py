475b1d70d57e219090e8ac016a6e202e0196d83d
siddharth-singh-bhadouria <sidsingh.singh01@gmail.com>
2023-08-16 15:16:54 +0530

    Added data

---
+++ Python/index.py
@@ -0,0 +1,21 @@
+
+set1 = set(['Apple', 'for', 'Apple'])
+print(set1)
+
+print(10/2)
+print(10/2.0)
+print((int)(10.5))
+print((int)(10.8))
+print(10//3)
+print(10/3)
+
+a = 10
+b = 20
+print(a and b)
+print(b and a)
+
+a = 10
+b = 4
+print(a & b)
+print(a | b)
+print(~a)
