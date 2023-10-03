f66443c738819a11a003bbf8193ef3274dec9325
siddharth-singh-bhadouria <sidsingh.singh01@gmail.com>
2023-10-03 15:27:14 +0530

    Array rotation with reverse function - Reverse array rotation

---
+++ Python Lab/reverseArrayRotation.py
@@ -0,0 +1,16 @@
+def reverse_array(arr, start, end):
+    while start < end:
+        arr[start], arr[end] = arr[end], arr[start]
+        start += 1
+        end -= 1
+
+def left_rotate_array(arr, k):
+    n = len(arr)
+    reverse_array(arr, 0, k - 1)
+    reverse_array(arr, k, n - 1)
+    reverse_array(arr, 0, n - 1)
+
+original_array = [1, 2, 3, 4, 5]
+k = 1  
+left_rotate_array(original_array, k)
+print(original_array)
