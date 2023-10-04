a76e36a64b28c38dd2cd9e75479cdb05565603dd
siddharth-singh-bhadouria <sidsingh.singh01@gmail.com>
2023-10-04 14:23:02 +0530

    Making changes to the reverse array rotation

--- Python Lab/reverseArrayRotation.py
+++ Python Lab/reverseArrayRotation.py
@@ -1,16 +1,15 @@
-def reverse_array(arr, start, end):
-    while start < end:
-        arr[start], arr[end] = arr[end], arr[start]
-        start += 1
-        end -= 1
-
-def left_rotate_array(arr, k):
+def reverse_rotate_array(arr, k):
     n = len(arr)
-    reverse_array(arr, 0, k - 1)
-    reverse_array(arr, k, n - 1)
-    reverse_array(arr, 0, n - 1)
+    k = k % n  
 
-original_array = [12, 21, 32, 43, 25]
-k = 1  
-left_rotate_array(original_array, k)
-print(original_array)
+    arr[:k] = arr[:k][::-1]
+
+    arr[k:] = arr[k:][::-1]
+
+    arr[:] = arr[::-1]
+
+my_array = [1, 2, 3, 4, 5]
+rotation_amount = 2
+
+reverse_rotate_array(my_array, rotation_amount)
+print(my_array)
