6e8f2faf70a62ff636b0a3b7163a99f15fd9c4da
siddharth-singh-bhadouria <sidsingh.singh01@gmail.com>
2023-10-13 14:30:46 +0530

    List difference and access index of list

---
+++ Python Lab/Other programs/accessIndex.py
@@ -0,0 +1,3 @@
+my_list = [10, 20, 30, 40, 50]
+for index, value in enumerate(my_list):
+    print(f"Index: {index}, Value: {value}")
---
+++ Python Lab/Other programs/listDifference.py
@@ -0,0 +1,4 @@
+list1 = [1, 2, 3, 4, 5]
+list2 = [3, 4, 5, 6, 7]
+difference_list = list(set(list1) - set(list2))
+print("Difference between list1 and list2 is:", difference_list)
