a5bdf12a5e6ba0e588b52887c0a0c76ecb454b47
siddharth-singh-bhadouria <sidsingh.singh01@gmail.com>
2023-10-13 14:34:46 +0530

    Unpack tuple and item index in tuple

---
+++ Python Lab/Other programs/itemIndexInTuple.py
@@ -0,0 +1,4 @@
+my_tuple = ('a', 'b', 'c', 'd', 'e')
+item_to_find = 'c'
+index = my_tuple.index(item_to_find)
+print(f"Index of {item_to_find} is {index}")
---
+++ Python Lab/Other programs/unpackTuple.py
@@ -0,0 +1,3 @@
+my_tuple = (1, 2, 3, "Hello", "World")
+var1, var2, var3, var4, var5 = my_tuple
+print(f"Unpacked variables: {var1}, {var2}, {var3}, {var4}, {var5}")
