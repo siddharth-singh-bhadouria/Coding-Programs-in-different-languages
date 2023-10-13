269334c974c72bac19692bd97474140fc05623c6
siddharth-singh-bhadouria <sidsingh.singh01@gmail.com>
2023-10-13 14:32:06 +0530

    char list to string and item index

---
+++ Python Lab/Other programs/charToString.py
@@ -0,0 +1,3 @@
+char_list = ['a', 'b', 'c', 'd', 'e']
+string_result = ''.join(char_list)
+print("Converted string:", string_result)
---
+++ Python Lab/Other programs/itemIndex.py
@@ -0,0 +1,4 @@
+specified_list = ['apple', 'banana', 'cherry', 'date', 'elderberry']
+item_to_find = 'cherry'
+index = specified_list.index(item_to_find)
+print(f"Index of {item_to_find} is {index}")
